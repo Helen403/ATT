f0505712ebca13fd48c3e0480ec7f4f1befa0579
Helen <helen@pengdemacbook-pro.local>
2016-12-15 14:45:58 +0800

    Initial Commit

---
+++ ATT考勤/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATT考勤
+//
+//  Created by Helen on 16/12/15.
+//  Copyright © 2016年 Helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATT考勤/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATT考勤
+//
+//  Created by Helen on 16/12/15.
+//  Copyright © 2016年 Helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
